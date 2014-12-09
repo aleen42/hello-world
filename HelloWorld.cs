d7ce475b6a42f03b644a3d551718aaecee8bf94f
Aleen <408503598@qq.com>
2014-12-09 23:25:42 +0800

    Create HelloWorld.cs

---
+++ HelloWorld.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace test_tool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HelloWorld");
+        }
+    }
+}
